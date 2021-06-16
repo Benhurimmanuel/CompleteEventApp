6abf2ea7d5a742b7b4a4a7c09ea3a859abbe6868
Benhurimmanuel <benhurimmanuel@gmail.com>
2021-06-16 18:11:17 +0530

    Update bookingsList.js

--- frontend/src/bookingsList.js
+++ frontend/src/bookingsList.js
@@ -1,31 +1,129 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import BookingsList from "../bookingsList";
 
-export default function BookingsList(props) {
+export default function Welcome() {
+  let [bookingsList, setBookingsList] = useState([]);
+  let [loading, setLoading] = useState(false);
+  const token = window.localStorage.getItem("app_token");
+  const user = window.localStorage.getItem("userId");
+
+  useEffect(() => {
+    bookingsfetch();
+  }, []);
+
+  console.log(bookingsList);
+  const bookingsfetch = () => {
+    console.log("in fetch");
+    setLoading(true);
+    const requestBody = {
+      query: `query {
+        bookings 
+          {_id
+         createdAt
+         event{
+           _id
+           title
+           date
+         }
+          }
+      }
+    `,
+    };
+    fetch("https://eventbookingappback.herokuapp.com/graphql  ", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log(resData.data.bookings);
+        setBookingsList(resData.data.bookings);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+  function handleCancel(id) {
+    // console.log(id);
+
+    setLoading(true);
+    const requestBody = {
+      query: `mutation {
+        cancelBooking (bookingId:"${id}")
+          {_id
+           title
+         }
+      }
+    `,
+    };
+    fetch("https://eventbookingappback.herokuapp.com/graphql  ", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        setBookingsList(resData.data.bookings);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }
   return (
     <>
       {""}
-      {props.bookings.map((item) => {
-        return (
+      <div className="container">
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : (
           <>
-            <tr key={item._id}>
-              <td>{item.event.title}</td>
-              <td>{new Date(item.event.date).toLocaleDateString()}</td>
-
-              <td>{new Date(item.createdAt).toLocaleDateString()}</td>
-
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={props.onDelete.bind(this, item._id)}
-                >
-                  Cancel
-                </button>
-              </td>
-            </tr>
-            {console.log(props.bookings[0].event)}
+            {" "}
+            <h2>Your Bookings</h2>
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Event</th>
+                  <th scope="col">Event Date</th>
+                  <th scope="col">Date of Booking</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {bookingsList.length ? (
+                  <BookingsList
+                    key="id"
+                    bookings={bookingsList}
+                    onDelete={handleCancel}
+                  ></BookingsList>
+                ) : (
+                  <p> data unavailable</p>
+                )}
+              </tbody>
+            </table>
           </>
-        );
-      })}
+        )}
+      </div>
     </>
   );
 }
